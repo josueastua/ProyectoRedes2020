24da6cd2f45212f2e741e6b044ea382e4b48ca1e
YohellBuzo <54456019+YohellBuzo@users.noreply.github.com>
2020-05-30 20:45:01 -0600

    codificar

--- TareaStackdeProtocolos/Presentacion.py
+++ TareaStackdeProtocolos/Presentacion.py
@@ -3,5 +3,30 @@ class Presentacion(object):
     def __init__(self, mensaje):
         self.mensaje = mensaje
 
-    def codificarMensaje(self):
-        mensajecod = ""
+    def Codificar(self):
+        codigo = ""
+        for carac in self.mensaje:
+            if ord(carac) <= 121:
+                codigo +=chr(ord(carac) + 5)
+            elif ord(carac)>= 122 and ord(carac) <= 126:
+                codigo +=chr(ord(carac) + 39)
+            elif ord(carac) >= 161:
+                codigo +=chr(ord(carac) + 5)
+        print(codigo)
+        self.mensaje = codigo
+
+    def Decoficar(self):
+        mensaje = ""
+        for carac in self.mensaje:
+            if ord(carac) <= 126:
+                mensaje +=chr(ord(carac) - 5)
+            elif ord(carac) >= 161 and ord(carac) <= 165:
+                mensaje +=chr(ord(carac) - 39)
+            elif ord(carac) >= 161:
+                mensaje +=chr(ord(carac) - 5)
+        print(mensaje)
+        self.mensaje = mensaje
+
+p = Presentacion("{Buenas noches árbol} | zzz ~")
+p.Codificar()
+p.Decoficar()
