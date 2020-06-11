10a0ec66c71ef62f849b74ec39516d10e0f2d443
YohellBuzo <54456019+YohellBuzo@users.noreply.github.com>
2020-06-11 11:52:14 -0600

    Dorso

--- Virus/src/virus/Controller/JuegoController.java
+++ Virus/src/virus/Controller/JuegoController.java
@@ -194,20 +194,15 @@ public class JuegoController extends Controller implements Initializable {
         AppContext.getInstance().set("Juego", FlowController.getInstance().getController("Juego"));
         player = (Jugador) AppContext.getInstance().get("Jugador");
         ArrayList<Jugador> jug = (ArrayList<Jugador>) AppContext.getInstance().get("Jugadores");
-        //conseguirImagenes();
+        conseguirImagenes();
         lblPlayer.setText(player.getId());
     }
 
     private void conseguirImagenes(){
         mazo = (ArrayList<String>) AppContext.getInstance().get("Mazo");
-        descartes = (ArrayList<String>) AppContext.getInstance().get("Mazo");
         mazo_img.clear();
-        descartes_img.clear();
         mazo.forEach((carta) -> {
             mazo_img.add(AppContext.getInstance().getCarta(carta));
-        });
-        descartes.forEach((carta) -> {
-            descartes_img.add(AppContext.getInstance().getCarta(carta));
         });
     }
     
